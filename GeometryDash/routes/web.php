3a7d43a5e793f0d7f3a5319a5bee8ba459c1842b
DrSkillers <skillersbombergrounds@gmail.com>
2023-07-24 10:47:37 +0200

    Route created

--- GeometryDash/routes/web.php
+++ GeometryDash/routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\IndexController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,10 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/', [IndexController::class,'show'])->name('pages.index');
