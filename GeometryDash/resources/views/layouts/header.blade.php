59ce57d193420bce1c82e2c4a56f0db114c8158e
DrSkillers <skillersbombergrounds@gmail.com>
2023-07-24 17:33:58 +0200

    my11threpo:D

---
+++ GeometryDash/resources/views/layouts/header.blade.php
@@ -0,0 +1,23 @@
+<div class="bg-dark"
+style="width: 95%; border-color: black; margin-left:2.5%;margin-top: 30px; border-radius:1rem ;padding-top: 10px; padding-left: 30px; padding-right: 30px;">
+<header
+    class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+    <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+        <img src="{{ asset('asset/images/logo.png') }}" alt="" class="class="bi me-2 width="60"
+            height="60" role="img" aria-label="Bootstrap" style="border-radius: 1rem;">
+    </a>
+
+    <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0" style="font-size: 19px;">
+        <li><a href="#" class="nav-link px-2">Home</a></li>
+        <li><a href="#" class="nav-link px-2">News</a></li>
+        <li><a href="#" class="nav-link px-2k">Community</a></li>
+        <li><a href="#" class="nav-link px-2">Threads</a></li>
+        <li><a href="#" class="nav-link px-2k">LevelGuessing</a></li>
+    </ul>
+
+    <div class="col-md-3 text-end">
+        <button type="button" class="btn btn-outline-primary me-2">Login</button>
+        <button type="button" class="btn btn-primary">Sign-up</button>
+    </div>
+</header>
+</div>
--- GeometryDash/resources/views/pages/index.blade.php
+++ GeometryDash/resources/views/pages/index.blade.php
@@ -1,2 +1,7 @@
 @extends('layouts.master')
 @section('title','index')
+@section('content')
+    <div class="light-500 mt-2 text-bold">
+        <h1 class="text-bold">Hello</h1>
+    </div>
+@endsection
