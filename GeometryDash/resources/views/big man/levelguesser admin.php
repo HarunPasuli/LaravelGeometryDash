2ce0ab33cd60d73fff9a20b1e820ff2715e18fff
DrSkillers <skillersbombergrounds@gmail.com>
2023-08-04 15:52:41 +0200

    big man

---
+++ GeometryDash/resources/views/big man/levelguesser admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+   <img src="guesser.png">
+   <h1> Admin </h1>
+   <p> css is my passion </p>
+   <form action="upload.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload"><br><br>
+  <h3> Name </h3>
+  <input type="text" name="name1" id="fileToUpload"><br><br>
+  <h3> AltName1 </h3>
+  <input type="text" name="name2" id="fileToUpload"><br><br>
+  <h3> AltName2 </h3>
+  <input type="text" name="name" id="fileToUpload"><br><br>
+  <h3> Difficulty: 1 - Easy | 2 - Medium | 3 - Hard </h3>
+  <input type="number" name="fileToUpload" id="fileToUpload"><br><br>
+  <input type="submit" value="Upload Image" name="submit">
+</body>
+
+
+<?php
+
+?>
+</html>
